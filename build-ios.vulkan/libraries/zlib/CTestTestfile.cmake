7ddf3907e08050d322bee407c28d6047f060c7c8
Yoshi Sugawara <yoshi.sugawara@gmail.com>
2023-03-17 23:15:14 -1000

    remove old build dir

--- build-ios.vulkan/libraries/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/yoshi/Code/personal/gzdoom/libraries/zlib
-# Build directory: /Users/yoshi/Code/personal/gzdoom/build-ios.vulkan/libraries/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
